f57bb3976424d79b6781914807b288fe904590d2
Victor220311 <victorfrancodb@gmail.com>
2025-10-01 10:51:24 +0000

    circulo 1

---
+++ Ejercicio_1/main.py
@@ -0,0 +1,8 @@
+#Getter: obtienen el valor de un atributo. Ejemplo:
+#get_diametro() para obtener el valor de self.diametro
+
+#Setter: modifican el valor de un atributo. Ejemplo:
+#set_diametro(nuevo.valor) para cambiar self.diametro
+
+#Comportamiento: metodos que definen acciones del objeto
+#ejemplo: girar(), frenar(), cambiar_color()
